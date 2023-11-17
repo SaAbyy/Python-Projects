f56fe0a55be8a7ff1fcd02e40daacdc4098fab89
SaAbyy <94359910+SaAbyy@users.noreply.github.com>
2023-11-17 14:27:59 +0100

    Update tcptest.py

--- tcptest.py
+++ tcptest.py
@@ -32,7 +32,54 @@ print('These datas were receives :' + datas)
 datas_d = zlib_d(datas)
 datas_d_clear = base64_d(datas_d)
 datas = send_datas(datas_d_clear)
-print('Datas uncompress :' + datas)
+print('Flag :' + datas)
 print('Deconnexion.')
 
 client.close()
+
+
+#########################################
+OR
+#########################################
+
+import socket; import base64; import zlib
+
+HOST = 'challenge01.root-me.org' # 212.129.38.224
+PORT = 52022
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+print('Connexion vers ' + HOST + ':' + str(PORT) + ' reussie.')
+
+def MsgSend(datas):
+    try:
+        client.send(datas)
+        return client.recv(1024)
+    except:
+        ValueError# print('Envoi de :' + datas)
+    # n = client.send(datas)
+    # if (n != len(datas)):
+    #     return 'Erreur envoi.'
+    # else:
+    #     return 'Envoi ok.', client.recv(1024)
+
+
+def zlib_decode(datas):
+    return zlib.decompress(datas)
+
+def b64_decode(datas):
+    return base64.b64decode(datas)
+
+def close():
+    client.close()
+
+while True :
+    user_input = input('Datas to send for connexion :')    
+    datas_receives = MsgSend(user_input) # Send message of user
+    print('Reception... \n' + datas_receives[1])
+    datas_b64_decode = b64_decode(datas_receives[1])
+  # Not finish yet
+
+
+
+
